3b393f6edf8bb124011db0a4191ea4c5947bfe1f
wangning <376885153@qq.com>
2020-12-08 10:10:43 +0800

    修改布局用flex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,14 +19,11 @@
       <!--      search bar-->
       <div class="search-bar">
         <el-row type="flex" align="middle">
-          <el-col :xs="{span: 0, offset: 0}" :sm="{span: 4, offset: 2}" :md="{span: 4, offset: 2}" :lg="{span: 4, offset: 2}" :xl="{span: 4, offset: 2}" class="hidden-xs-only col-main">
             <img class="hidden-xs-only" src="@/assets/logobig.png" alt="shopping image">
-          </el-col>
-          <el-col :xs="{span: 24, offset: 0}" :sm="{span: 12, offset: 0}" :md="{span: 12, offset: 0}" :lg="{span: 12, offset: 0}" :xl="{span: 12, offset: 0}">
+
             <el-input clearable="true" placeholder="请输入内容" v-model="input" class="input">
               <el-button v-waves slot="append" icon="el-icon-search">搜索</el-button>
             </el-input>
-          </el-col>
         </el-row>
       </div>
 <!--      shop nav-->
